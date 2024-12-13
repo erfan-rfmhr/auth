75069ff6edbb8a0e65e0203e9bd870332b382a05
Erfan Arefmehr <erfan.arefmehr@gmail.com>
2024-12-13 13:03:23 +0330

    FEAT: Add user urls to config

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView, SpectacularAPIView
 
 urlpatterns = [
@@ -21,6 +21,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    path("user/", include("user.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
